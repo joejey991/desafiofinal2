c4a4c42b9b0f02adbe6e19c71043f27e5b9de8f9
Jorge Herrera <jorgeherreraotarola@gmail.com>
2024-06-02 16:55:49 -0400

    integración del componente alert en el componente formulario para el proceso de validacion del formulario, se modifica el index.html -cambio de titulo -incluyo el cdn de bootstrap

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,64 @@
 import React from "react";
 import { useState } from "react";
-
+import Alert from "./Alert";
 
 const Formulario = () => {
     const [userName, setUserName] = useState("");
     const [userEmail, setUserEmail] = useState("");
     const [userPassword, setUserPassword] = useState("");
     const [userRetryPassword, setUserRetryPassword] = useState("");
+
+    const [userNameAlert, setUserNameAlert] = useState("");
+    const [userEmailAlert, setUserEmailAlert] = useState("");
+    const [userPasswordAlert, setUserPasswordAlert] = useState("");
+    const [userRetryPasswordAlert, setUserRetryPasswordAlert] = useState("");    
     
     const validateForm = (e) => {
         e.preventDefault();
-        alert('validar el formulario')
-    }
+        let valid = true;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+        if (userName === "") {
+          setUserNameAlert("Debes completar el campo de nombre");
+          valid = false;
+        } else if (userName.length < 2) {
+          setUserNameAlert("El largo mínimo del nombre no se cumple");
+          valid = false;
+        } else {
+          setUserNameAlert("");
+        }
+    
+        if (userEmail === "") {
+          setUserEmailAlert("Debes completar el campo de email");
+          valid = false;
+        } else if (!emailPattern.test(userEmail)) {
+          setUserEmailAlert("El formato del email no es correcto");
+          valid = false;
+        } else {
+          setUserEmailAlert("");
+        }
+    
+        if (userPassword === "") {
+          setUserPasswordAlert("Debes completar el campo de contraseña");
+          valid = false;
+        } else {
+          setUserPasswordAlert("");
+        }
+    
+        if (userRetryPassword === "") {
+          setUserRetryPasswordAlert("Debes confirmar tu contraseña");
+          valid = false;
+        } else if (userPassword !== userRetryPassword) {
+          setUserRetryPasswordAlert("Las contraseñas deben ser iguales");
+          valid = false;
+        } else {
+          setUserRetryPasswordAlert("");
+        }
+    
+        if (valid) {
+          alert("Datos ingresados correctamente");
+        }
+      };
     return (
         <>
           <h5>O usa tu email para registrarte</h5>
@@ -25,6 +72,7 @@ const Formulario = () => {
                 value={userName}
                 onChange={(e) => setUserName(e.target.value)}
               />
+              {userNameAlert && <Alert message={userNameAlert} type="danger" />}
             </div>
             <div className="mb-3">
               <input
@@ -35,6 +83,7 @@ const Formulario = () => {
                 value={userEmail}
                 onChange={(e) => setUserEmail(e.target.value)}
               />
+              {userEmailAlert && <Alert message={userEmailAlert} type="danger" />}
             </div>
             <div className="mb-3">
               <input
@@ -45,6 +94,7 @@ const Formulario = () => {
                 value={userPassword}
                 onChange={(e) => setUserPassword(e.target.value)}
               />
+              {userPasswordAlert && (<Alert message={userPasswordAlert} type="danger" />)}
             </div>
             <div className="mb-3">
               <input
@@ -55,6 +105,7 @@ const Formulario = () => {
                 value={userRetryPassword}
                 onChange={(e) => setUserRetryPassword(e.target.value)}
               />
+              {userRetryPasswordAlert && (<Alert message={userRetryPasswordAlert} type="danger" />)}
             </div>
             <button type="submit" className="btn btn-success form-control">
               Registrarse
